a95bd418f67a9a851443d38f14b5d9b08d8687c6
100153234 <100153234@lubbockisd.net>
2023-09-14 09:50:44 -0500

    WyattNall

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+//Wyatt Nall
+//9/14/23
+
+
+import java.util.Scanner;
+public class Main
+{
+    public static void main (String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+        String StringOne;
+        int intOne;
+
+        System.out.print("What is your first name :: ");
+        StringOne = keyboard.next();
+        System.out.println("Your name is :  " + StringOne);
+
+        System.out.print("What is your age :: ");
+        intOne = keyboard.nextInt();
+
+
+
+        System.out.println("Your age is :  " + intOne);
+
     }
 }
